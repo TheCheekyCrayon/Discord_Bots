32469a3a417a5b375ebd4e406e7111f80b9a5ced
Wesley Ray <Raywes88@gmail.com>
2018-08-26 00:59:00 -0400

    Documented code a bit; also limited $say command to advanced auth.

--- Ricky/src/main/java/tutorial/Ricky/RickyReloaded.java
+++ Ricky/src/main/java/tutorial/Ricky/RickyReloaded.java
@@ -43,13 +43,14 @@ public class RickyReloaded extends ListenerAdapter {
 	 * Jared =
 	 */
 	private static final long WES_SNOWFLAKE = 321798967669030912L;
-	private static final long[] advanced_commands_auth = {WES_SNOWFLAKE};
-	public static final char command_symbol = '$';
-	public static String[] quotes;
+	private static final long[] ADVANCED_COMMANDS_AUTH = {WES_SNOWFLAKE};
+	private static final char COMMAND_SYMBOL = '$';
     private static final String FEED_URL = "https://www.youtube.com/feeds/videos.xml?channel_id=UCkOlmd_lMI9YHRcN1ffKbyQ";
-    private static List<String> seen;
-    private static Random rand;
-    private static String quotes_path;
+    
+    private static String[] quotes;		//Contains currently loaded quotes
+    private static List<String> seen;	//Contains list of seen video URLs; videos in this list will not be sent to channel
+    private static Random rand;			
+    private static String quotes_path;	//File system path to text file containing quotes to load; this is loaded from the properties file
 
 	public static void main(String[] args) throws IOException, ClassNotFoundException, LoginException, IllegalArgumentException, InterruptedException, RateLimitedException {
 		Properties bot_conf = loadConfig(args[0]);
@@ -65,12 +66,15 @@ public class RickyReloaded extends ListenerAdapter {
        		//if the file doesn't exist just create a new ArrayList
        		RickyReloaded.seen = new ArrayList<String>();
        	}
-       
+
+		//Load quotes from disk
 		load_quotes_from_file();
     	
+		//Create Discord bot object; this estavlishes connection to discord server etc..
    		final JDA rickyBot = new JDABuilder(AccountType.BOT).setToken(bot_conf.getProperty("ricky")).buildBlocking();
         rickyBot.addEventListener(new RickyReloaded());
         
+        //Set up task to check Ricky's yt channel for new videos
         Timer timer = new Timer ();
         TimerTask rickyVideoFeedUpdate = new TimerTask () {
             @Override
@@ -98,8 +102,12 @@ public class RickyReloaded extends ListenerAdapter {
     @Override
     public void onMessageReceived(MessageReceivedEvent e) {
     	String message = e.getMessage().getContent();
+    	
+    	//We don't want to do anything with message that don't contain text... for now
     	if(message.length() == 0)
     			return;
+    	
+    	//Various useful references we'll need
     	MessageChannel objChannel = e.getChannel();
     	User objUser = e.getAuthor();
     	Guild objGuild = e.getGuild();
@@ -108,15 +116,15 @@ public class RickyReloaded extends ListenerAdapter {
     	//Use this to find out user unique ID
     	//System.out.println(objUser.getName() + ": " +objUser.getIdLong());
     	
-    	if(message.charAt(0) == command_symbol) {
+    	//If the message begins with the command symbol pass it to the handleCommand function and return
+    	if(message.charAt(0) == COMMAND_SYMBOL) {
     		handleCommand(message, objChannel, objUser, objGuild, ricardo);
     		return;
     	}
     	
-    	float chance = RickyReloaded.rand.nextFloat();
-
-    	if(objChannel.getName().equalsIgnoreCase("general_discussion")){
-    		if (chance <= 0.05f){
+    	//Ricky's random interjections
+    	if(objChannel.getName().equalsIgnoreCase("general_discussion")){					//Limit his outbursts to general_discussion
+    		if (RickyReloaded.rand.nextFloat() <= 0.05f){									//shooting for 5% of the time
     			int randomIndex = RickyReloaded.rand.nextInt(RickyReloaded.quotes.length);
     			objChannel.sendMessage(RickyReloaded.quotes[randomIndex]).queue();
     		}
@@ -126,18 +134,22 @@ public class RickyReloaded extends ListenerAdapter {
 	private void handleCommand(String message, MessageChannel objChannel, User objUser, Guild objGuild, JDA richard) {
 		String[] strArgs = message.substring(1).split(" ");
 		
+		//Pick a random quote and send to channel; mentions command issuer
 		if(strArgs[0].equalsIgnoreCase("quote")){
 			Random rand = new Random();
 			int randomIndex = rand.nextInt(RickyReloaded.quotes.length);
 			objChannel.sendMessage(RickyReloaded.quotes[randomIndex] + " " + objUser.getAsMention()).queue();
-			
+		
+		//Send link to subscribe to Ricky's channel; mentions command issuer
 		} else if(strArgs[0].equals("subscribe")){	// pastes the link to subscribe in the chat
 			objChannel.sendMessage("https://www.youtube.com/channel/UCkOlmd_lMI9YHRcN1ffKbyQ?sub_confirmation=1 " + objUser.getAsMention() + " Now like and subscribe!").queue();
 			
-		} else if(strArgs[0].equalsIgnoreCase("say")) {
+		//Makes Ricky say things in general_discussion; Can be spammy, limited to advanced auth users
+		} else if(advanced_auth(objUser.getIdLong()) && strArgs[0].equalsIgnoreCase("say")) {
 			TextChannel chan = richard.getTextChannelsByName("general_discussion", true).get(0);
 			chan.sendMessage(message.substring(message.indexOf(" "))).queue();
 			
+		//Lists available commands in chat
 		} else if(strArgs[0].equalsIgnoreCase("commands")) {	// sends messages of the commands the bot uses
 			String msg = "In the #help channel:\n\n";
 			msg += "$DJ    (for the DJ role, @DJ )\n";
@@ -149,6 +161,7 @@ public class RickyReloaded extends ListenerAdapter {
 			msg += "In any channel $subscibe will generate a link that allows you to subscribe to the Cookingwith Rick YouTube channel\n";
 			msg += "$quote will grab a random quote that Rick has said himself\n\n";			
 			objChannel.sendMessage(msg).queue();
+			
 		} else if(strArgs[0].equals("DJ") && objChannel.getName().equalsIgnoreCase("help")) {
 			setRole("DJ", objGuild, objUser, objChannel);
 			
@@ -166,9 +179,10 @@ public class RickyReloaded extends ListenerAdapter {
 			
 		} else if(strArgs[0].equals("Member") && objChannel.getName().equalsIgnoreCase("help")) {
 			setRole("Member", objGuild, objUser, objChannel);
-			
+		
+		//This command is pretty spammy so it's limited to users listed in the advanced auth list at the top of the program
+		//This code will combine 10 quotes per message separated by a newline so we don't get rate limited
 		} else if(advanced_auth(objUser.getIdLong()) && strArgs[0].equals("list_quotes")) {
-			//This code will combine 10 quotes per message separated by a newline so we don't get rate limited
 			String msg = "Listing " + RickyReloaded.quotes.length + " quotes:\n";
 			for (int i = 0; i < RickyReloaded.quotes.length; i++) {
 				msg += RickyReloaded.quotes[i] + "\n";
@@ -180,9 +194,9 @@ public class RickyReloaded extends ListenerAdapter {
 				}
 			}
 			
+		//This command is limited to just Wes since no one else has access to the quotes file anyway
+		//Also it causes disk activity on server
 		} else if(objUser.getIdLong() == WES_SNOWFLAKE && strArgs[0].equals("reload_quotes")) {
-			//This command is limited to just Wes since no one else has access to the quotes file anyway
-			//Also it causes disk activity on server
 			try {
 				objChannel.sendMessage("Attempting to reload quotes from " + RickyReloaded.quotes_path).queue();
 				load_quotes_from_file();
@@ -237,6 +251,11 @@ public class RickyReloaded extends ListenerAdapter {
 		return null;
 	}
 	
+	/**
+	 * Broke out code that loads quotes from file into a helper fuction
+	 * to support the $reload_quotes command
+	 * @throws IOException
+	 */
 	private static void load_quotes_from_file() throws IOException {
        	FileInputStream fs = null;
    		fs = new FileInputStream(RickyReloaded.quotes_path);
@@ -249,6 +268,11 @@ public class RickyReloaded extends ListenerAdapter {
    		array.toArray(RickyReloaded.quotes);
 	}
 	
+	/**
+	 * Super janky function that parses video urls from a youtube channel rss feed.
+	 * @param urlAddress - url of the feed itself
+	 * @param chan - Channel to send video links to
+	 */
     public static void readRSSFeed(String urlAddress, TextChannel chan){
         Pattern r = Pattern.compile("href=\"(.+)\"");
         try{
@@ -280,8 +304,15 @@ public class RickyReloaded extends ListenerAdapter {
         }
     }
     
+    /**
+     * Java is gay af and doesn't have a good way to check if an array of primitives
+     * contains a specific value...
+     * 
+     * @param id - User's snowflake id to check
+     * @return - true if id is contained in advanced_commands_auth
+     */
     private static boolean advanced_auth(long id) {
-    	for(long l : advanced_commands_auth) {
+    	for(long l : ADVANCED_COMMANDS_AUTH) {
     		if(l == id)
     			return true;
     	}
